d63013c08c71e5c0943f90db5cd669668d51be68
Alexey Tsivunin <most-208@yandex.ru>
2023-10-27 17:13:10 +0400

    Add and configure ESLint (#5)
    Parent story: https://github.com/fenya123/bingin/issues/9
    
    As a part of adding linters configuration story we need to add and configure eslint.
    
    We try to work with strictest configuration as possible, so we have to enable all available linter rules, and disable only those we don't actually need.
    
    In the scope of this task we need to add and configure eslint rules and refactor existing code to comply with the rules.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: "app-root",
+  standalone: true,
+  templateUrl: "./app.component.html",
 })
 export class AppComponent { }
