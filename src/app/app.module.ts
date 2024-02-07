3ec1cd7aecfe1c978bde7e71ec4bfde3bcaf5f8a
Alexey Bobrov <55196139+NOneNice@users.noreply.github.com>
2024-02-07 23:57:14 +0500

    add Credentials Interceptor for transfer credentials between endpoints

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,19 @@ import { AppComponent } from './app.component';
 import { ModulesModule } from './modules/modules.module';
 import { RouterOutlet } from '@angular/router';
 import { PagesModule } from './pages/pages.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { CredentialsInterceptor } from './intercepter/credentials.interceptor';
 
 @NgModule({
   bootstrap: [AppComponent],
   imports: [BrowserModule, ModulesModule, PagesModule, RouterOutlet],
   declarations: [AppComponent],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: CredentialsInterceptor,
+      multi: true,
+    },
+  ],
 })
 export class AppModule {}
