9a81f549d18d871e8aad77363c24ed4a8f0f64a9
Alexey Bobrov <q20100@mail.ru>
2024-02-11 18:37:46 +0500

    fix gap between inputs

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,3 @@ platformBrowserDynamic()
     // eslint-disable-next-line no-console
     console.error(err);
   });
-
-// TODO: поправить отступы на форме логина (между лого и инпутами, между инпутами, над кнопкой, в сообщениях об ошибках)
-// TODO: уменьшить шрифт ошибок
-// TODO: увеличить шрифт sign up
-// TODO: добавить prettier в package.json и package-lock.json
-// TODO: заменить кавычки на двойные
