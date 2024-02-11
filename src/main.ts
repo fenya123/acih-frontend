a6167c6985ab994b765a38ea7000fee88c1b603f
Alexey Bobrov <q20100@mail.ru>
2024-02-11 13:01:18 +0500

    fix shadow and brightness on img

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { AppModule } from "./app/app.module";
 
-
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
-  .catch(err => { console.error(err); }); // eslint-disable-line no-console
+  .catch(err => {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  });
 
-
+// TODO: поправить отступы на форме логина (между лого и инпутами, между инпутами, над кнопкой, в сообщениях об ошибках)
+// TODO: уменьшить шрифт ошибок
+// TODO: увеличить шрифт sign up
+// TODO: добавить prettier в package.json и package-lock.json
+// TODO: заменить кавычки на двойные
